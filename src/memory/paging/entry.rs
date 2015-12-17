133e808e9e7b4a70cc82c11296f94e5d2c5552e3
Philipp Oppermann <dev@phil-opp.com>
2015-12-17 18:42:53 +0100

    Use AtomicUsize for entries because it has side effects
    AtomicUsize prevents instruction reordering. The `Ordering` arguments are preliminary set to `SeqCst`, maybe they can be weakened in the future.

--- src/memory/paging/entry.rs
+++ src/memory/paging/entry.rs
@@ -1,23 +1,24 @@
 use memory::Frame;
+use core::sync::atomic::{AtomicUsize, Ordering};
 
-pub struct Entry(u64);
+pub struct Entry(AtomicUsize);
 
 impl Entry {
     pub fn is_unused(&self) -> bool {
-        self.0 == 0
+        self.0.load(Ordering::SeqCst) == 0
     }
 
     pub fn set_unused(&mut self) {
-        self.0 = 0;
+        self.0.store(0, Ordering::SeqCst);
     }
 
     pub fn flags(&self) -> EntryFlags {
-        EntryFlags::from_bits_truncate(self.0)
+        EntryFlags::from_bits_truncate(self.0.load(Ordering::SeqCst))
     }
 
     pub fn pointed_frame(&self) -> Option<Frame> {
         if self.flags().contains(PRESENT) {
-            Some(Frame::containing_address(self.0 as usize & 0x000fffff_fffff000))
+            Some(Frame::containing_address(self.0.load(Ordering::SeqCst) & 0x000fffff_fffff000))
         } else {
             None
         }
@@ -25,12 +26,12 @@ impl Entry {
 
     pub fn set(&mut self, frame: Frame, flags: EntryFlags) {
         assert!(frame.start_address() & !0x000fffff_fffff000 == 0);
-        self.0 = (frame.start_address() as u64) | flags.bits();
+        self.0.store(frame.start_address() | flags.bits(), Ordering::SeqCst);
     }
 }
 
 bitflags! {
-    flags EntryFlags: u64 {
+    flags EntryFlags: usize {
         const PRESENT =         1 << 0,
         const WRITABLE =        1 << 1,
         const USER_ACCESSIBLE = 1 << 2,
