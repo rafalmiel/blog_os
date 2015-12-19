c2f5b1107883c1be836581e08aacfa60d854646c
Philipp Oppermann <dev@phil-opp.com>
2015-12-19 15:57:28 +0100

    Update `init` function (it works now)

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -45,28 +45,51 @@ pub fn init<A>(allocator: &mut A, boot_info: &BootInformation)
 
     let elf_sections_tag = boot_info.elf_sections_tag().expect("Memory map tag required");
 
-    let mut active_table = unsafe { RecursivePageTable::new() };
+    let mut active_table = unsafe { RecursivePageTable::new(allocator) };
     let mut new_table = {
         let frame = allocator.allocate_frame().expect("no more frames");
         InactivePageTable::new_on_identity_mapped_frame(frame, &mut active_table)
     };
 
     active_table.with(&mut new_table, |table| {
-        for section in elf_sections_tag.sections().filter(|s| s.flags & 0x2 != 0) {
+        for section in elf_sections_tag.sections() {
+            if section.flags & 0x2 == 0 {
+                // section is not loaded to memory
+                continue
+            }
+
             println!("mapping section at addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
                      section.addr,
                      section.size,
                      section.flags);
+
+            let mut flags = EntryFlags::empty();
+            if section.flags & 0x1 != 0 {
+                flags = flags | WRITABLE;
+            }
+            if section.flags & 0x4 == 0 {
+                // TODO set NXE bit in EFER
+                // flags = flags | NO_EXECUTE;
+            }
+
             let range = Range {
                 start: section.addr as usize,
                 end: (section.addr + section.size) as usize,
             };
             for address in range.step_by(PAGE_SIZE) {
                 let frame = Frame::containing_address(address);
-                let flags = WRITABLE | NO_EXECUTE;
                 table.identity_map(frame, flags, allocator);
             }
         }
-        println!("...done")
+        println!("...done");
+        println!("{:?}", table.translate(0));
+        println!("{:?}", table.translate(0xb8000));
+        println!("{:?}", table.translate(0x100000));
+        println!("{:?}", table.translate(0x10b3e3));
     });
+
+    let old_table = active_table.swap(new_table);
+    // TODO unmap old p4 table and turn it into a guard page for the kernel stack
+    // TODO make Page Copy and Page::containing_address public?
+    //active_table.unmap(Page::containing_address(old_table.start_address()));
 }
